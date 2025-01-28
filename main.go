658fd94bb32b4c052dc53b008b87796d02029b0f
devasherr <ashersam116@gmail.com>
2025-01-28 06:02:06 -0900

    atleast it works - keyboard layout

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/devasherr/terminal_music/notes"
 	"github.com/eiannone/keyboard"
 	"github.com/hajimehoshi/oto/v2"
@@ -43,6 +44,63 @@ func main() {
 	var player oto.Player
 	duration := 300
 
+	whiteKeyStyle := lipgloss.NewStyle().
+		Width(4).
+		Height(10).
+		Align(lipgloss.Center, lipgloss.Bottom).
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		Background(lipgloss.Color("15"))
+
+	blackKeyStyle := lipgloss.NewStyle().
+		Width(2).
+		Height(6).
+		Align(lipgloss.Center, lipgloss.Bottom).
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		Background(lipgloss.Color("0"))
+
+	whiteKeyPressedStyle := whiteKeyStyle.Background(lipgloss.Color("220"))
+	blackKeyPressedStyle := blackKeyStyle.Background(lipgloss.Color("220"))
+
+	whiteKeys := []string{"a", "s", "d", "f", "g", "h", "j"}
+	blackKeys := []string{"w", "e", "", "t", "y", "u"}
+
+	keyStyles := make(map[rune]lipgloss.Style)
+	for i, key := range whiteKeys {
+		keyStyles[rune(key[0])] = whiteKeyStyle
+		if i < len(blackKeys) && blackKeys[i] != "" {
+			keyStyles[rune(blackKeys[i][0])] = blackKeyStyle
+		}
+	}
+
+	// State of each key
+	pressedKeys := make(map[rune]bool)
+
+	renderPiano := func() string {
+		var pianoRow []string
+		for i, key := range whiteKeys {
+			style := whiteKeyStyle
+			if pressedKeys[rune(key[0])] {
+				style = whiteKeyPressedStyle
+			}
+			pianoRow = append(pianoRow, style.Render(key))
+
+			if i < len(blackKeys) && blackKeys[i] != "" {
+				style := blackKeyStyle
+				if pressedKeys[rune(blackKeys[i][0])] {
+					style = blackKeyPressedStyle
+				}
+				pianoRow = append(pianoRow, style.Render(blackKeys[i]))
+			}
+		}
+		return lipgloss.JoinHorizontal(lipgloss.Top, pianoRow...)
+	}
+
+	fmt.Print("\033[H\033[2J")
+	fmt.Print("\033[?25l")
+	fmt.Println(renderPiano())
+
 	fmt.Println("Press ESC to quit")
 	for {
 		char, key, err := keyboard.GetKey()
@@ -59,9 +117,22 @@ func main() {
 			player.Close()
 		}
 
-		wave := generateSineWave(notes.CharToNote[char], time.Millisecond*time.Duration(duration))
-		player = ctx.NewPlayer(bytes.NewReader(wave))
-		player.Play()
+		if freq, ok := notes.CharToNote[char]; ok {
+			wave := generateSineWave(freq, time.Millisecond*time.Duration(duration))
+			player = ctx.NewPlayer(bytes.NewReader(wave))
+			player.Play()
+
+			pressedKeys[char] = true
+
+			fmt.Print("\033[H")
+			fmt.Println(renderPiano())
+
+			time.Sleep(100 * time.Millisecond)
+
+			pressedKeys[char] = false
+			fmt.Print("\033[H")
+			fmt.Println(renderPiano())
+		}
 
 		lock.Unlock()
 	}
